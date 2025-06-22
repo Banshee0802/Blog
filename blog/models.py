c1b80ded73c23a933ca39bf0cb8b4862ee127b4d
Morrigan <ms.netty@inbox.ru>
2025-06-22 12:45:14 +0300

    Создала модель поста и зарегистрировала в админке

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True) # автоматический счет времени
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        db_table = 'blog_posts'
+
+    def __str__(self):   # функция для отображения названия поста
+        return self.title
+    
+
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,4 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse('Главная страница')
+
