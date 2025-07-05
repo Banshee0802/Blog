2f42e930f3350991a7b7cfb8fb14f15a1447e18b
Morrigan <ms.netty@inbox.ru>
2025-07-05 12:42:04 +0300

    Добавили функционал для добавления поста(самый простой вариант)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_post_list, get_post_detail    # импорт вьюшки
+from .views import get_post_list, get_post_detail, create_post    # импорт вьюшки
 
 urlpatterns = [
     path('posts/', get_post_list, name='post_list'),
-    path('posts/<int:id>/', get_post_detail, name='get_post_detail')
+    path('posts/<int:id>/', get_post_detail, name='get_post_detail'),
+    path('posts/add', create_post, name='new_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
@@ -6,7 +6,6 @@ def get_post_list(request):
     posts = Post.objects.all() # Импорт всех постов из бд и сохранение в переменной
 # если заменить all на order_by('created_at'), то будет сортировка по дате создания
 # если написать -created_at, то будет сортировка в обратном порядке
-
     return render(request, 'python/post_list.html', context={'posts': posts})
 
 
@@ -17,3 +16,15 @@ def get_post_detail(request, id):
     context = {'post': post}
 
     return render(request, 'python/post_detail.html', context)
+
+
+def create_post(request):
+    if request.method == 'GET':
+        return render(request, 'python/post_add.html')
+    
+    if request.method == 'POST':
+
+        post = Post.objects.create(title=request.POST.get('title'), content=request.POST.get('content'))
+
+        return redirect('get_post_detail', id=post.id)
+    
