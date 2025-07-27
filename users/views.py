39446f20e13a18eae3b2311b28fc06648a42971e
Morrigan <ms.netty@inbox.ru>
2025-07-27 12:17:29 +0300

    реализация фунционала входа в аккаунт и выход

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == 'POST':
@@ -11,9 +12,29 @@ def register_view(request):
         else:
             return render(request, 'users/register.html', {'form': form})
 
-    
     form = UserCreationForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            return redirect('post_list')
+        else:
+            return render(request, 'users/login.html', {'form': form})
+
+    form = AuthenticationForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('post_list')
 
 
